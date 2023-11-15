23761b8dd61de4f735a44105016aa23115e97d8a
hieu.dd.tlu@gmail.com <hieu.dd.tlu@gmail.com>
2023-11-15 23:54:16 +0700

    day_13

---
+++ src/main/java/api_learning/IFrameInteraction.java
@@ -0,0 +1,42 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import support.ui.SelectEX;
+
+public class IFrameInteraction {
+    //Declare constants
+    private static final String TARGET_URL = "https://the-internet.herokuapp.com/iframe";
+    private static final By IFRAME_SEL = By.cssSelector("iframe[id^='mce']");
+
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getWebDriver();
+        driver.get(TARGET_URL);
+
+        try {
+            WebElement iframeEle = driver.findElement(IFRAME_SEL);
+
+
+            //Switch to the target iframe
+            driver.switchTo().frame(iframeEle);
+
+            //Interact with iframe's elems
+            WebElement inputFieldEle = driver.findElement(By.id("tinymce"));
+            inputFieldEle.click();
+            inputFieldEle.clear();
+            inputFieldEle.sendKeys("Babilala");
+            Thread.sleep(2000);
+
+            //Switch back to the parent frame
+            driver.switchTo().defaultContent();
+
+            //Continue to interact with other eles outside the frame
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
